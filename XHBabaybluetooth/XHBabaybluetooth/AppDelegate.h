88508826f608d135e153d835ff9e088af88ecf40
pengwei <pengwei6@asiainfo.com>
2019-11-20 14:43:57 +0800

    初始化项目

---
+++ XHBabaybluetooth/XHBabaybluetooth/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  XHBabaybluetooth
+//
+//  Created by power on 2019/11/20.
+//  Copyright © 2019 Henan XinKangqiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ XHBabaybluetooth/XHBabaybluetooth/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XHBabaybluetooth
+//
+//  Created by power on 2019/11/20.
+//  Copyright © 2019 Henan XinKangqiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
